fe16838b89212e7a5e671cdc0788494c5551fdf2
madhava reddy <madhavaster@gmail.com>
2018-02-05 21:51:19 +0530

    ok

--- ExecutorsFrameWork1/src/com/madhav/FirstWay.java
+++ ExecutorsFrameWork1/src/com/madhav/FirstWay.java
@@ -2,12 +2,16 @@ package com.madhav;
 
 public class FirstWay {
 	public static void main(String[] args) {
-		FirstTask ft = new FirstTask();
-		ft.start();
+		/*FirstTask ft = new FirstTask();
+		ft.start();*/
+		new FirstTask();
 	}
 }
 
 class FirstTask extends Thread{
+	FirstTask(){
+		this.start();
+	}
 	@Override
 	public void run() {
 		for(int i=1;i<=10;i++) {
